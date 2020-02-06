a523f8223b44d2f13ab72330e334b7154e93757a
hubishan <hubishan@shein.com>
2020-02-06 11:03:21 +0800

    kwd

---
+++ word_vector_training.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# @Title     : TODO
+# @Objective : TODO
+# @Time      : 2020/2/6 11:02
+# @Author    : hubishan
+# @Site      :
+# @File      : word_vector_training.py
+# @Software  : IntelliJ IDEA
+
+import os
+import sys
